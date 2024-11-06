008a804badba4e69119e8f2d3b3764eb8c7ce250
Eric Ackermann <eric.ackermann@cispa.de>
2024-11-06 08:27:40 +0100

    Sync board files

--- soc/openhwgroup/cv64a6/cv64a6/soc_cache_management.c
+++ soc/openhwgroup/cv64a6/cv64a6/soc_cache_management.c
@@ -6,6 +6,7 @@
  */
 #include <stddef.h>
 #include <zephyr/arch/cache.h>
+#include <zephyr/kernel.h>
 
 void arch_dcache_enable(void)
 {
@@ -15,4 +16,10 @@ void arch_dcache_enable(void)
 void arch_dcache_disable(void)
 {
 	__asm__ volatile("csrwi 0x7C1, 0x00");
+}
+
+/* FIXME there is no common implementation for RISC-V, so we provide a SoC-level definition */
+void __weak arch_cache_init(void)
+{
+
 }
